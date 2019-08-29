65c4ebbe6c952d748f8c341e9dc37a30d6126c9b
WANGFENG <825528411@qq.com>
2019-08-29 20:22:50 +0800

    mysql建库语句

---
+++ SSM.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.5.40 : Database - ssm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssm`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `uid` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `money` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+insert  into `account`(`uid`,`id`,`money`) values (11,1,5000),(1,2,3000),(2,3,4000),(3,4,2000),(3,5,6000),(4,6,7000),(11,7,10000),(11,9,3000),(11,11,3000),(7,12,8000),(9,13,7000),(9,14,7000),(11,15,7000),(14,16,3000),(17,19,55555);
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `adminname` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`adminname`,`password`,`id`) values ('admin','admin',0);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `age` int(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`age`) values (1,'zhangsan','123',23),(2,'lisi','234',19),(3,'wangwu','678',22),(4,'zhouliu','890',18),(5,'wangfeng','222',26),(7,'zhansghufang','234',51),(9,'王小林','234',52),(11,'王峰','1992',28),(12,'李燕','520',26),(13,'张三','123',26),(14,'李四','lisi2',18),(16,'高健','2222',22),(17,'钻石王老五','555',55);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
